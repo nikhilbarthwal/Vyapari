4eef6e21289549e2c5829058e68504d48aee245b
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2025-09-30 18:25:37 +0530

    rename

--- Vyapari.Tests/Utils.fs
+++ Vyapari.Tests/Utils.fs
@@ -5,11 +5,11 @@ open Vyapari
 
 module Utils =
 
-    let internal Test<'T> (f: 'T -> bool) (v: 'T list): bool =
+    let Test<'T> (f: 'T -> bool) (v: 'T list): bool =
         let check b (x: 'T): bool = if (f x) then b else false
         List.fold check true v
 
-    let internal Bar (random: System.Random) (t: time) =
+    let Bar (random: System.Random) (t: time) =
         DataPoint(ask = 3.0 + random.NextDouble(),
                   bid = 1.0 + random.NextDouble(),
                   time = t,
