761c5efaa27afda1b26efdbe035b83c87e93b017
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2025-09-30 18:39:23 +0530

    Enhancing Tests (#12)

--- Vyapari.Tests/Utils.fs
+++ Vyapari.Tests/Utils.fs
@@ -5,12 +5,16 @@ open Vyapari
 
 module Utils =
 
-    let internal Test<'T> (f: 'T -> bool) (v: 'T list): bool =
+    let Test<'T> (f: 'T -> bool) (v: 'T list): bool =
         let check b (x: 'T): bool = if (f x) then b else false
         List.fold check true v
 
-    let internal Bar (random: System.Random) (t: time) =
+    let Bar (random: System.Random) (t: time) =
         DataPoint(ask = 3.0 + random.NextDouble(),
                   bid = 1.0 + random.NextDouble(),
                   time = t,
                   volume = 0)
+
+    let Gen (length: int) (min: int, max: int): Array<int> =
+        let random = System.Random(System.Guid.NewGuid().GetHashCode())
+        Array.Initialize(length, fun _ -> random.Next(min, max))
