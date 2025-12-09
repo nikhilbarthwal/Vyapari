1d386809cdae1dfefd1dc259dd4a0c255d0d7b3e
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2025-12-09 12:25:22 -0800

    .

--- Vyapari/Buffer.fs
+++ Vyapari/Buffer.fs
@@ -3,71 +3,94 @@ namespace Vyapari
 open Vyapari
 
 
-module Buffer =
+module LinearBuffer =
+    
+    type Adapter<'T> =
+        abstract BucketCount: int
+        abstract Interval: time
+        abstract Merge<'T>: int -> int -> 'T -> 'T -> 'T
+        abstract Init<'T>: unit -> 'T
+        abstract Update<'T>: time -> 'T -> 'T
+        abstract Extrapolate: 'T -> 'T -> int -> time -> time -> int -> 'T
 
-    type private Bucket<'T>(init: unit -> 'T, merge: int -> 'T -> 'T -> 'T) =
-        let mutable data: 'T = init()
+    
+    module Bisect =
+        let inline internal Decimal (r1: int) (r2: int)
+                                    (v1: decimal) (v2: decimal) =
+            (v1 * (decimal r1) + v2 * (decimal r2)) / (decimal <| r1 + r2)
+
+        let inline internal Long (r1: int) (r2: int) (v1: int64) (v2: int64) =
+            (v1 * (int64 r1) + v2 * (int64 r2)) / (int64 <| r1 + r2)
+
+
+    type private Bucket<'T when 'T :> Data<'T>>(adapter: Adapter<'T>) =
+        let mutable data: 'T = adapter.Init()
         let mutable count = 0
 
         member this.Data = assert (count > 0) ; data
-        member this.Reset() = data <- init() ; count <- 0
+        member this.Reset() = data <- adapter.Init() ; count <- 0
         member this.Count = count
         member this.Add(x: 'T) =
-            if count > 0 then (data <- merge count data x) else (data <- x)
+            printfn $"Bucket Insert {x} with Count {count}"
+            if count > 0 then (data <- adapter.Merge 1 count x data) else (data <- x)
             count <- count + 1
+            printfn $"Bucket Insert {x} with Count {count} AFTER\n\n"
+        static member Create(adapter: Adapter<'T>) (_:int) = Bucket(adapter)
 
-    type private Buckets<'T when 'T :> Data<'T>>(bucketCount: int,
-                                                 init: unit -> 'T,
-                                                 merge: int -> 'T -> 'T -> 'T) =
+    type private Buckets<'T when 'T :> Data<'T>>(adapter: Adapter<'T>) =
         let mutable pos = 0
-        let buckets = Array.Initialize(bucketCount, fun _ -> Bucket(init, merge))
-        let index k = (pos + k) % bucketCount
-        do assert (bucketCount > 1)
+        let buckets = Array.Initialize(adapter.BucketCount, Bucket.Create(adapter))
+        let index k = (pos + k) % adapter.BucketCount
+        do assert (adapter.BucketCount > 1)
 
         member this.Item with get(k: int) = buckets[index(k)]
         member this.Previous() = buckets[pos].Data.Time
         member this.Shift(k) =
             assert (k > 0)
             for i in [1 .. k] do buckets[index <| i-1].Reset()
-            pos <- (pos + k) % bucketCount
+            pos <- (pos + k) % adapter.BucketCount
 
         member this.Reset() =
-            pos <- 0 ; for i in [0 .. bucketCount - 1] do buckets[i].Reset()
+            pos <- 0 ; for i in [0 .. adapter.BucketCount - 1] do buckets[i].Reset()
 
-    type internal Queue<'T when 'T :> Data<'T>>(
-        output: 'T -> unit,
-        bucketCount: int,
-        interval: time,
-        init: unit -> 'T,
-        merge: int -> 'T -> 'T -> 'T,
-        extrapolate: 'T -> 'T -> int -> time -> time -> int -> 'T) =
+    type internal Queue<'T when 'T :> Data<'T>>(adapter: Adapter<'T>, output: 'T -> unit) =
 
-        let buckets = Buckets(bucketCount, init, merge)
+        let buckets = Buckets(adapter)
         let mutable previous: time = 0L
-        let floor (t:time) = t - (t % (int64 interval))
+        let floor (t:time) = t - (t % (int64 adapter.Interval))
 
         let extrapolate (diff: int): unit =
+            printfn $"Extrapolate {diff}"
 #if DEBUG
             assert (diff > 0)
-            assert (buckets[0].Count > 0)
             assert (buckets[diff].Count = 1)
+            assert(buckets[0].Count > 0)
             if diff > 1 then
                 for k in [1 .. diff - 1] do assert(buckets[k].Count = 0)
 #endif
-            let eval = extrapolate (buckets[diff].Data) (buckets[0].Data)
-                                   diff previous interval
-            for k in [0 .. diff - 1] do (output <| eval k)
+            let eval = adapter.Extrapolate (buckets[diff].Data) (buckets[0].Data)
+                                            diff previous adapter.Interval
+            for k in [0 .. diff - 1] do
+                let z = eval k
+                printfn $"Output is {z}"
+                output z
+            (* for k in [0 .. diff - 1] do
+                let z = adapter.Merge k (diff - k) (buckets[0].Data) (buckets[diff].Data)
+                printfn $"Output is {z}"
+                output <| adapter.Merge k (diff - k) (buckets[0].Data) (buckets[diff].Data) *)
 
         interface BufferQueue<'T> with
             member this.Ingest(input: 'T): bool =
                 let current = floor input.Time
                 if buckets[0].Count = 0 then // Initial case
+                    printfn $"Insert {input} with floor {current} INITIAL"
                     buckets[0].Add input
                     previous <- current ; true
                 else
                     assert (input.Time >= buckets.Previous())
-                    let diff = int <| (current - previous) / interval
-                    if diff >= bucketCount then
+                    let diff = int <| (current - previous) / adapter.Interval
+                    printfn $"Insert {input} with floor {current} / Diff is {diff}"
+                    if diff >= adapter.BucketCount then
                         buckets.Reset()
                         buckets[0].Add input
                         previous <- current ; false
--- Vyapari/DataPoint.fs
+++ Vyapari/DataPoint.fs
@@ -19,27 +19,38 @@ type DataPoint = { Ask: decimal; Bid: decimal; Time: time; Volume: int64} with
 module DataPoint =
 
     let Init() = { Ask = 0m ; Bid = 0m ; Time = 0L ; Volume = 0L }
-    let Prices(length: int) = Data.Price<DataPoint>(length, Init)
+    let Array(length: int) = Data.Price<DataPoint>(length, Init)
 
-    let private merge (count: int) (data: DataPoint) (x: DataPoint) =
-        let avgFloat = Utils.BisectFloat count 1
-        let avgLong = Utils.BisectLong count 1
-        { Ask = avgFloat data.Ask x.Ask
-          Bid = avgFloat data.Bid x.Bid
-          Time = avgLong data.Time x.Time
-          Volume = avgLong data.Volume x.Volume }
+    type Buffer(interval: time, bucketCount: int) =
+        let adapter: LinearBuffer.Adapter<DataPoint> =
+            { new LinearBuffer.Adapter<DataPoint> with 
+                member this.BucketCount = bucketCount
+                member this.Interval = interval
+                member this.Merge r1 r2 x1 x2 =
+                    let avgDecimal = LinearBuffer.Bisect.Decimal r1 r2
+                    let avgLong = LinearBuffer.Bisect.Long r1 r2
+                    { Ask = avgDecimal x1.Ask x2.Ask
+                      Bid = avgDecimal x1.Bid x2.Bid
+                      Time = avgLong x1.Time x2.Time
+                      Volume = avgLong x1.Volume x2.Volume }
 
-    let private extrapolate (curr: DataPoint) (prev: DataPoint) (diff: int)
-                    (previous: time) (interval: time) (k: int) =
-        let extrapolateFloat = Utils.BisectFloat k <| diff - k
-        let extrapolateLong = Utils.BisectLong k <| diff - k
-        { Ask = extrapolateFloat curr.Ask prev.Ask
-          Bid = extrapolateFloat curr.Bid prev.Bid
-          Time = previous + interval * (int64 k)
-          Volume = extrapolateLong curr.Volume prev.Volume }
+                member this.Init() = DataPoint.Init()
 
-    type Buffer(interval, bucketCount) =
+                member this.Update time x =
+                    { Ask = x.Ask ; Bid = x.Bid ; Time = time ; Volume = x.Volume }
+                    
+                member this.Extrapolate (curr: DataPoint) (prev: DataPoint) (diff: int)
+                                          (previous: time) (interval: time) (k: int) =
+                    let extrapolateDecimal = LinearBuffer.Bisect.Decimal k <| diff - k
+                    let extrapolateLong = LinearBuffer.Bisect.Long k <| diff - k
+                    { Ask = extrapolateDecimal curr.Ask prev.Ask
+                      Bid = extrapolateDecimal curr.Bid prev.Bid
+                      Time = previous + interval * (int64 k)
+                      Volume = extrapolateLong curr.Volume prev.Volume }
+            }
+        
+ 
         interface Buffer<DataPoint> with
             member this.Initialize() = Init()
             member this.BufferQueue(insert): BufferQueue<DataPoint> =
-                Buffer.Queue(insert, bucketCount, interval, Init, merge, extrapolate)
+                LinearBuffer.Queue(adapter, insert)
